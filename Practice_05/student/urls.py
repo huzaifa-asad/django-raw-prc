113f8d303b3ee854db4d7537ce0c6a1c70940ee4
huzaifa-asad <khuzaifaasad55@gmail.com>
2025-09-02 12:26:05 +0500

    create superuser for admin panel

--- Practice_05/student/urls.py
+++ Practice_05/student/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from student.views import all_data
+from student.views import all_data, single_data
 
 
 urlpatterns = [
-    path('all/', all_data, name='all_data')
+    path('all/', all_data, name='all_data'),
+    path('single/', single_data, name='single_data'),
 ]
--- Practice_05/student/views.py
+++ Practice_05/student/views.py
@@ -6,3 +6,11 @@ def all_data(req):
     all_students = Profile.objects.all()
     print(all_students)
     return render(req, 'student/all.html', {'students': all_students})
+
+
+def single_data(req):
+    # student = Profile.objects.get(id=1)
+    # student = Profile.objects.get(name='hamza')
+    student = Profile.objects.get(pk=1)
+    print(student)
+    return render(req, 'student/single.html', {'student': student})
