cb18249daed122ed0b40e1a026a5d046810682df
huzaifa-asad <khuzaifaasad55@gmail.com>
2025-09-02 14:05:38 +0500

    register model in admin panel

--- Practice_05/student/models.py
+++ Practice_05/student/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 class Profile(models.Model):
     name = models.CharField(max_length=70)
+    roll = models.IntegerField()
     email = models.EmailField(max_length=255)
     city = models.CharField(max_length=70)
     
-    
+    def __str__(self):
+        # return self.name
+        return str(self.roll)
