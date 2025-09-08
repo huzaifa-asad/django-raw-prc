ce22bdc52d7c29a9ac6b86ebef833b339625adbf
huzaifa-asad <khuzaifaasad55@gmail.com>
2025-09-08 11:15:51 +0500

    register site in admin panel

--- Practice_05/student/admin.py
+++ Practice_05/student/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from student.models import Profile
+from student.models import Profile, Results
 
 # Register your models here.
 admin.site.register(Profile)
+
+
+# Create modelAdmin class for Results
+@admin.register(Results) #short form of site register
+class ResultsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'student_class', 'marks')
+# admin.site.register(Results, ResultsAdmin)
