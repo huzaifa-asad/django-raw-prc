3f08ec8d1fbed81942435cd20da7ae16b9808b21
huzaifa-asad <khuzaifaasad55@gmail.com>
2025-09-11 19:39:08 +0500

    Save and Update Data into database

--- Practice_07/student/views.py
+++ Practice_07/student/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render
 from student.forms import Registration
 from django.http import HttpResponseRedirect
+from student.models import Profile
 
 def register(req):
     if req.method == 'POST':
         form = Registration(req.POST)
         if form.is_valid():
             # print(form.cleaned_data)
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-            print('Name:', name)
-            print('Email:', email)
-            print('Password:', password)
+            nm = form.cleaned_data['name']
+            em= form.cleaned_data['email']
+            pw = form.cleaned_data['password']
+            print('Name:', nm)
+            print('Email:', em)
+            print('Password:', pw)
+            
+            # Save Data into Database
+            user = Profile(name = nm, email=em, password=pw)
+            user.save()
+            
+            # Update Data into Database
+            # user = Profile(id=1, name=nm, email=em, password=pw)
+            # user.save()
+            
             # to save from resubmission form
             return HttpResponseRedirect('/student/success/')
     else:
