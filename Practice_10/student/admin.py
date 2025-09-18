74c91266d8aa185471b9b5de75344281d7296150
huzaifa-asad <khuzaifaasad55@gmail.com>
2025-09-18 23:40:50 +0500

    Form mastering wirh details validation and everything

--- Practice_10/student/admin.py
+++ Practice_10/student/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from student.models import Profile
 
 # Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = [
+            'name', 'dob', 'gender', 'locality', 'city', 'pin', 'state',
+            'mobile', 'email', 'job_city', 'profile_img', 'mv_file'
+        ]
+    
