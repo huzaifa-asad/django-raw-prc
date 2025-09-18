5fbac6f91c5a631d429921f3e1e1a00d0b1831d9
huzaifa-asad <khuzaifaasad55@gmail.com>
2025-09-18 23:41:18 +0500

    Form mastering wirh details validation and everything

--- Practice_10/student/views.py
+++ Practice_10/student/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from student.forms import ProfileForm
 from student.models import Profile
 
 
 def home(request):
-    form = ProfileForm()
-    return render(request, 'student/home.html', {'form': form})
+    candidates = Profile.objects.all()
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ProfileForm()
+    return render(request, 'student/home.html', {'form': form, 'candidates': candidates})
+
+
+def candidate_detail(request, id):
+    candidate = Profile.objects.get(id=id)
+    return render(request, 'student/candidates.html', {'candidate': candidate})
