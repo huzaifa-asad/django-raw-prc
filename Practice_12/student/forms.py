9fbcfa903dd61b88b5cd800226f2c2492e6d83b7
huzaifa-asad <khuzaifaasad55@gmail.com>
2025-09-20 10:46:58 +0500

    Display Error and Success Messages

---
+++ Practice_12/student/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from student.models import User
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['name', 'email', 'password']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
