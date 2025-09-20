4451576062377e282c74d9f8e6fd0567905b19bb
huzaifa-asad <khuzaifaasad55@gmail.com>
2025-09-20 10:52:22 +0500

    Register the model

--- Practice_12/student/admin.py
+++ Practice_12/student/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from student.models import User
 
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'email', 'password')
     search_fields = ('name', 'email')
