ddf734c61d37cabe1613ce6355a5441a686ff687
Huzaifa Asad <khuzaifaasad55@gmail.com>
2025-09-21 11:43:51 +0500

    Update fixes

--- Practice_04/core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from core.views import home, about
-
-urlpatterns = [
-    path('', home, name="home"),
-    path('about/', about, name="about")
-]
--- Practice_04/core/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def home(req):
-    return render(req, 'core/home.html')
-
-def about(req):
-    return render(req, 'core/about.html')
