47f399ed374420431a88182e5ebfd2659e204184
huzaifa-asad <khuzaifaasad55@gmail.com>
2025-10-04 07:25:36 +0500

    Default Signals & Custom Signal

--- Practice_18/blogSignals/views.py
+++ Practice_18/blogSignals/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import HttpResponse
-
+from blogSignals import signals
 # Create your views here.
 
 def home(request):
     a = 10/0
     return HttpResponse('Hello')
+
+def custom_signal(request):
+    signals.notification.send(sender=None, request=request, user=["Huzaifa", "Asad"])
+    return HttpResponse('This is Custom Signal')
