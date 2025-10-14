52bc76d20ea0cef22102e04b68575f89249397b3
huzaifa-asad <khuzaifaasad55@gmail.com>
2025-10-14 23:41:11 +0500

    Working with Database Fields lookups

---
+++ Practice_22/school/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from school.models import Student
+
+# Create your views here.
+def home(request):
+    # student_data = Student.objects.all()
+    # student_data = Student.objects.filter(name="Talha")
+    # student_data = Student.objects.filter(name__exact="Talha")
+    
+    # if not case sensitive use i means insensitive
+    # student_data = Student.objects.filter(name__iexact="Talha")
+    # student_data = Student.objects.filter(name__contains="a")
+    # student_data = Student.objects.filter(name__icontains="S")
+    
+    # student_data = Student.objects.filter(id__in=[1, 4, 3])
+    
+    # Greater than
+    # student_data = Student.objects.filter(marks__gt=60)
+    # Greater than or Equal to
+    # student_data = Student.objects.filter(marks__gte=74)
+    # Less than 
+    # student_data = Student.objects.filter(marks__lt=74)
+    # Less than or Equal to
+    # student_data = Student.objects.filter(marks__lte=74)
+    
+    # student_data = Student.objects.filter(name__startswith="h")
+    # student_data = Student.objects.filter(name__istartswith="h")
+    # student_data = Student.objects.filter(name__endswith="a")
+    
+    # Range between two dates
+    student_data = Student.objects.filter(pass_date__range=("2023-10-1", "2025-10-12"))
+    print('Return: ', student_data)
+    return render(request, 'school/home.html', {'students': student_data})
