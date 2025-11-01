30a4284b4f17fb38c09d2d97471d0b63031351ae
huzaifa-asad <khuzaifaasad55@gmail.com>
2025-11-01 21:36:40 +0500

    Sync and Async View in Django

--- Practice_32/AsyncViews/urls.py
+++ Practice_32/AsyncViews/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path
-from core.views import home
+from core.views import home, sync_view, async_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('sync/', sync_view, name='sync_view'),
+    path('async/', async_view, name='async_view')
 ]
