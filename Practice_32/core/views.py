f02af2e5b4771d6d4b49279b8ce034ca5b6b5032
huzaifa-asad <khuzaifaasad55@gmail.com>
2025-11-01 21:36:55 +0500

    Sync and Async View in Django

--- Practice_32/core/views.py
+++ Practice_32/core/views.py
@@ -1,8 +1,44 @@
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
+import httpx
+import time
+import asyncio
 
 def home(request):
     return HttpResponse('Hello Home Page')
 
+# async def home(request):
+#     return HttpResponse('Hello Home Page')
 
-async def home(request):
-    return HttpResponse('Hello Home Page')
+# Synchronous View
+def sync_view(request):
+    start_time = time.time()
+    responses = []
+    for _ in range(5):
+        response = httpx.get("https://jsonplaceholder.typicode.com/posts")
+        responses.append(response.json())
+    end_time = time.time()
+    time_taken = end_time - start_time
+    return JsonResponse({
+        'status': 'Success',
+        'total_request': 5,
+        'time_taken': f"{time_taken: .2f} seconds"
+    })
+    
+
+# Asynchronous View
+async def async_view(request):
+    start_time = time.time()
+    
+    async with httpx.AsyncClient() as client:
+        tasks = [client.get("https://jsonplaceholder.typicode.com/posts")
+        for _ in range(5)]
+        responses = await asyncio.gather(*tasks)
+        
+    end_time = time.time()
+    time_taken = end_time - start_time
+    return JsonResponse({
+        'status': 'Success',
+        'total_request': 5,
+        'time_taken': f"{time_taken: .2f} seconds"
+    })
