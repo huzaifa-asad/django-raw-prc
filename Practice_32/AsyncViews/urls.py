fe0a6f1891a4d1c48ba7833a2f10604817f8fbce
huzaifa-asad <khuzaifaasad55@gmail.com>
2025-11-17 15:54:33 +0500

    Fix an issue in urls.py

--- Practice_32/AsyncViews/urls.py
+++ Practice_32/AsyncViews/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('sync/', sync_view, name='sync_view'),
-    path('async/', async_view, name='async_view')
+    path('async/', async_view, name='async_view'),
 ]
