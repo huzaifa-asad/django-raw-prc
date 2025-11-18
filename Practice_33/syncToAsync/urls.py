f453a86cfcdb4d6774b6f748a2054354844c27c7
huzaifa-asad <khuzaifaasad55@gmail.com>
2025-11-18 22:22:58 +0500

    Sync to Async and Async to Sync Methods

--- Practice_33/syncToAsync/urls.py
+++ Practice_33/syncToAsync/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from core.views import student_data
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('student/', student_data),
 ]
 
 if settings.DEBUG:
