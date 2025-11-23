f32a070c8b25a8395094d71d753d8ce099f7e5dd
huzaifa-asad <khuzaifaasad55@gmail.com>
2025-11-23 21:28:23 +0500

    django Async ORM

--- Practice_35/asyncORM/asgi.py
+++ Practice_35/asyncORM/asgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asyncORM.settings')
 
-application = get_asgi_application()
+application = ASGIStaticFilesHandler(get_asgi_application)
