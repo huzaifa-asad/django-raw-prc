e913101fc1c87b87e6ef6a2a4c8e5c344b1f3588
huzaifa-asad <khuzaifaasad55@gmail.com>
2025-12-13 23:38:26 +0500

    django class based views

--- Practice_37/core/views.py
+++ Practice_37/core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+def myfunview1(request):
+    return HttpResponse("Hello Function based View")
+
+def myfunview2(request):
+    return HttpResponse("<h1>Function Based View</h1>")
+
+def homefunview(request):
+    return render(request, "core/home.html")
+
+def aboutfunview(request):
+    context = {'msg': 'Welcome to GeekyShows Function Based View'}
+    return render(request, 'core/about.html', context)
