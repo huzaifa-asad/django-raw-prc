e814507f608a0e1ed1ef2b2aa2189b27dd7d629d
huzaifa-asad <khuzaifaasad55@gmail.com>
2026-01-08 00:00:02 +0500

    Generic Class based view in django

--- Practice_39/core/views.py
+++ Practice_39/core/views.py
@@ -1,1 +1,8 @@
 from django.shortcuts import render
+from django.views import View
+from core.models import Student
+
+class AllStudentView(View):
+    def get(self, request):
+        all_students = Student.objects.all()
+        return render(request, 'core/all_student.html', {'all_students': all_students})
