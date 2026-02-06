56eeede3028e1492de8ce177dab99fc8e53b4ddd
huzaifa-asad <khuzaifaasad55@gmail.com>
2026-02-06 23:25:21 +0500

    pagination using function based views

---
+++ Practice_40/core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Post
+from django.core.paginator import Paginator
+
+# Create your views here.
+def post_list(request):
+    all_post = Post.objects.all().order_by('publish_date')
+    paginator = Paginator(all_post, per_page=3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    print('Page Number ', page_number)
+    print('Page Obj ', page_obj)
+    return render(request, 'core/home.html', {'page_obj': page_obj})
