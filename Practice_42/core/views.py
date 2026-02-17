b495cb84943d8d3aca663787b58dd1ac6b11ea04
huzaifa-asad <khuzaifaasad55@gmail.com>
2026-02-17 23:58:08 +0500

    Async  class based view

---
+++ Practice_42/core/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, HttpResponse, JsonResponse
+from django.views import View
+import httpx
+import time
+import asyncio
+
+# Create your views here.
+class HomeView(View):
+    async def get(self, request, *args, **kwargs):
+        return HttpResponse("Hello Home Page")
+    
+# Synchronous Class-Based View
+class SyncView(View):
+    def get(self, request, *args, **kwargs):
+        start_time = time.time()
+        responses = []
+        
+        for _ in range(5):
+            response = httpx.get("https://jsonplaceholder.typicode.com/posts")
+            responses.append(response.json())
+            
+        end_time = time.time()
+        time_taken = end_time - start_time
+        return JsonResponse({
+            'status': 'success',
+            'view_type': 'sync',
+            'total_request': 5,
+            'time_taken': f"{time_taken:.2f} seconds"
+        })
+        
+        
+# Asynchronous Class_Based View
+class AsyncView(View):
+    async def get(self, request, *args, **kwargs):
+        start_time = time.time()
+        
+        async with httpx.AsyncClient() as client:
+            tasks = [client.get("https://jsonplaceholder.typicode.com/posts") for _ in range(5)]
+            responses = await asyncio.gather(*tasks)
+            
+        end_time = time.time()
+        time_taken = end_time - start_time
